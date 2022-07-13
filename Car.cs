67114d5036e930e50daf7b926acce9caa6ed6b79
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-13 15:40:20 -0400

    Finished exercise todos

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise;
+
+internal class Car
+{
+    public string Make{ get; set; } 
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+
+
+
+
+    public void PrintCarInfo()
+    {
+        Console.WriteLine("Car Info: ");
+        Console.WriteLine($"Make: {Make}");
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine("This car is sweet!");
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassesExercise;
+#region Car
+{
+
+    var car1 = new Car();
+
+    car1.Make = "Ford";
+
+    car1.Model = "Mustang";
+
+    car1.Year = 1997;
+
+    car1.PrintCarInfo();
+
+
+
+
+}
+
+
+
+
+
+#endregion
